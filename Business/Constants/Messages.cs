7569c80b636d6c08cfc11f851c57ce0eee655b42
emir_ <emir_kokum_@hotmail.com>
2024-02-16 11:42:21 +0300

    10.th ep finished

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product added";
+        public static string ProductNameInvalid = "Invalid product name";
+        internal static string MaintenanceTime = "It is maintenance time";
+        internal static string ProductsListed = "Products has listed ";
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 
 class Program
 {
@@ -32,9 +33,20 @@ class Program
     {
         ProductManager productManager = new ProductManager(new EfProductDal());
 
-        foreach (var product in productManager.GetProductDetails())
+        var result = productManager.GetProductDetails();
+
+        if (result.Success == true)
         {
-            Console.WriteLine(product.ProductName + " -- " + product.CategoryName);
+            foreach (var product in result.Data)
+            {
+                Console.WriteLine(product.ProductName + " -- " + product.CategoryName);
+            }
         }
+        else
+        {
+            Console.WriteLine(result.Message);
+        }
+
+
     }
 }
